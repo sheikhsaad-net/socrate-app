68b2c2b964f051b79420e0a5750e99dffd1b5384
Sheikh Saad <sheikhsaadsd@gmail.com>
2025-07-31 11:32:13 +0200

    assetsUpdates

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,23 +6,23 @@
           <div class="row">
             <div class="col-md-6">
               <div class="position-absolute w-40 top-0 start-0 h-100 d-md-block d-none">
-                <div class="oblique-image position-absolute d-flex fixed-top ms-auto h-100 z-index-0 bg-cover me-n8" style="background-image:url('../assets/img/image-sign-up.jpg')">
+                <div class="oblique-image position-absolute d-flex fixed-top ms-auto h-100 z-index-0 bg-cover me-n8" style="background-image:url('{{ url('/assets/img/image-sign-up.jpg') }}">
                   <div class="my-auto text-start max-width-350 ms-7">
                     <h1 class="mt-3 text-white font-weight-bolder">Il tuo percorso <br> personale nella psicologia.</h1>
                     <p class="text-white text-lg mt-4 mb-4">Inserisci i tuoi dati per accedere a valutazioni psicologiche personalizzate, monitorare i tuoi progressi ed esplorare approfondimenti selezionati per la tua chiarezza mentale.</p>
                     <div class="d-flex align-items-center">
                       <div class="avatar-group d-flex">
                         <a class="avatar avatar-sm rounded-circle" data-bs-toggle="tooltip" data-original-title="Jessica Rowland">
-                          <img alt="Image placeholder" src="../assets/img/team-3.jpg" class="">
+                          <img alt="Image placeholder" src="{{ url('/assets/img/team-3.jpg') }}" class="">
                         </a>
                         <a class="avatar avatar-sm rounded-circle" data-bs-toggle="tooltip" data-original-title="Audrey Love">
-                          <img alt="Image placeholder" src="../assets/img/team-4.jpg" class="rounded-circle">
+                          <img alt="Image placeholder" src="{{ url('/assets/img/team-4.jpg') }}" class="rounded-circle">
                         </a>
                         <a class="avatar avatar-sm rounded-circle" data-bs-toggle="tooltip" data-original-title="Michael Lewis">
-                          <img alt="Image placeholder" src="../assets/img/marie.jpg" class="rounded-circle">
+                          <img alt="Image placeholder" src="{{ url('/assets/img/marie.jpg') }}" class="rounded-circle">
                         </a>
                         <a class="avatar avatar-sm rounded-circle" data-bs-toggle="tooltip" data-original-title="Audrey Love">
-                          <img alt="Image placeholder" src="../assets/img/team-1.jpg" class="rounded-circle">
+                          <img alt="Image placeholder" src="{{ url('/assets/img/team-1.jpg') }}" class="rounded-circle">
                         </a>
                       </div>
                       <p class="font-weight-bold text-white text-sm mb-0 ms-2">Unisciti a oltre 2.5K+ utenti</p>
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -7,19 +7,19 @@
   <meta charset="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <link rel="apple-touch-icon" sizes="76x76" href="../assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="apple-touch-icon" sizes="76x76" href="{{ url('/assets/img/apple-icon.png') }}">
+  <link rel="icon" type="image/png" href="{{ url('/assets/img/favicon.png') }}">
   <title>{{ config('app.name', 'Laravel') }}</title>
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700|Noto+Sans:300,400,500,600,700,800|PT+Mono:300,400,500,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="../assets/css/nucleo-icons.css" rel="stylesheet" />
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{ url('/assets/css/nucleo-icons.css') }}" rel="stylesheet" />
+  <link href="{{ url('/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/349ee9c857.js" crossorigin="anonymous"></script>
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="{{ url('/assets/css/nucleo-svg.css') }}" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="../assets/css/corporate-ui-dashboard.css?v=1.0.0" rel="stylesheet" />
+  <link id="pagestyle" href="{{ url('/assets/css/corporate-ui-dashboard.css?v=1.0.0') }}" rel="stylesheet" />
 
    <!-- Scripts -->
    @vite(['resources/css/app.css', 'resources/js/app.js'])
@@ -34,10 +34,10 @@
 
         @livewireScripts
     <!--   Core JS Files   -->
-    <script src="../assets/js/core/popper.min.js"></script>
-    <script src="../assets/js/core/bootstrap.min.js"></script>
-    <script src="../assets/js/plugins/perfect-scrollbar.min.js"></script>
-    <script src="../assets/js/plugins/smooth-scrollbar.min.js"></script>
+    <script src="{{ url('/assets/js/core/popper.min.js') }}"></script>
+    <script src="{{ url('/assets/js/core/bootstrap.min.js') }}"></script>
+    <script src="{{ url('/assets/js/plugins/perfect-scrollbar.min.js') }}"></script>
+    <script src="{{ url('/assets/js/plugins/smooth-scrollbar.min.js') }}"></script>
     <script>
         var win = navigator.platform.indexOf('Win') > -1;
         if (win && document.querySelector('#sidenav-scrollbar')) {
@@ -50,6 +50,6 @@
     <!-- Github buttons -->
     <script async defer src="https://buttons.github.io/buttons.js"></script>
     <!-- Control Center for Corporate UI Dashboard: parallax effects, scripts for the example pages etc -->
-    <script src="../assets/js/corporate-ui-dashboard.min.js?v=1.0.0"></script>
+    <script src="{{ url('/assets/js/corporate-ui-dashboard.min.js?v=1.0.0') }}"></script>
     </body>
 </html>
